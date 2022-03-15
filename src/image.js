eabfc765d89676b8ca8d16a398dafc88dde96750
Bill Robbins <bill.robbins@automattic.com>
2022-03-15 13:56:31 -0500

    Used CSS to help with drag and drop images.

--- src/image.js
+++ src/image.js
@@ -3,15 +3,18 @@ import { addImage } from './DataStore';
 
 export const ImageUpload = (props) => {
 	const [selectedImage, setselectedImage] = useState();
+	const [uploading, setUploading] = useState('waiting');
 	const fileRef = useRef();
 
 	const setImage = async (e) => {
 		const image = e.target.files[0];
+		setUploading('loading');
 		const formData = new FormData();
 		formData.append('file', image);
 		formData.append('title', image.name);
 		formData.append('type', image.type);
 		const resp = await addImage(formData);
+		setUploading('waiting');
 		props.setImageID(resp.id);
 		setselectedImage(URL.createObjectURL(image));
 	};
@@ -22,8 +25,14 @@ export const ImageUpload = (props) => {
 	}, [props.changed]);
 
 	return (
-		<div className="image-uploader">
+		<div className={'image-uploader ' + uploading}>
 			<input type="file" name="file" onChange={setImage} ref={fileRef} />
+			<p className="image-upload-instructions">
+				<span className="default">
+					Click to select a file, or drag it here.
+				</span>
+				<span className="loading-image">Uploading image.</span>
+			</p>
 			{selectedImage && <img src={selectedImage} alt="" />}
 		</div>
 	);
