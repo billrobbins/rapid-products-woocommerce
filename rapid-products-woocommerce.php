b753b6c279d594d5a7b3ec0f4c3f8a2b71acd95a
Bill Robbins <bill.robbins@automattic.com>
2022-03-28 17:11:40 -0500

    Updating with WordPress PHP Coding Standards

--- rapid-products-woocommerce.php
+++ rapid-products-woocommerce.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 /**
  * Plugin Name:       Rapid Products for WooCommerce
  * Plugin URI:        https://github.com/billrobbins/rapid-products-woocommerce
@@ -13,26 +12,29 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
- // Load Admin Page
-require_once plugin_dir_path( __FILE__ ) . '/includes/class-admin-page.php';
+// Load Admin Page.
+require_once plugin_dir_path( __FILE__ ) . '/includes/class-rapid-product-wc-admin-page.php';
 
- // Load REST Endpoint for Options
- require_once plugin_dir_path( __FILE__ ) . '/includes/class-endpoints.php';
+// Load REST Endpoint for Options.
+require_once plugin_dir_path( __FILE__ ) . '/includes/class-rapid-products-wc-rest-controller.php';
 
- /**
+/**
  * Register and enqueue JS and CSS
  */
 function rapid_products_load_scripts() {
 	if ( isset( $_GET['page'] ) && ! empty( $_GET['page'] ) && 'rapid-products' !== $_GET['page'] ) {
 		return;
 	}
-	
+
 	$script_path       = '/build/index.js';
 	$script_asset_path = dirname( __FILE__ ) . '/build/index.asset.php';
 	$script_asset      = file_exists( $script_asset_path )
-		? require( $script_asset_path )
-		: array( 'dependencies' => array(), 'version' => filemtime( $script_path ) );
-	$script_url = plugins_url( $script_path, __FILE__ );
+		? require $script_asset_path
+		: array(
+			'dependencies' => array(),
+			'version'      => filemtime( $script_path ),
+		);
+	$script_url        = plugins_url( $script_path, __FILE__ );
 
 	wp_register_script(
 		'rapid-products',
@@ -45,7 +47,6 @@ function rapid_products_load_scripts() {
 	wp_register_style(
 		'rapid-products',
 		plugins_url( '/build/index.css', __FILE__ ),
-		// Add any dependencies styles may have, such as wp-components.
 		array(),
 		filemtime( dirname( __FILE__ ) . '/build/index.css' )
 	);
